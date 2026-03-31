9043e72b5bf373978a7b446930cebb47835b7c60
Albert <ab71433@hotmail.com>
2026-03-31 15:49:54 +0800

    feat: configure GitHub Pages deployment workflow and set base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/order-book/',
   plugins: [vue()],
   test: {
     globals: true,
