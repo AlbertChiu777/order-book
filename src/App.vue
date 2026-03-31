e6f32dda314024b18d2f6f289080631b76510c2d
Albert <ab71433@hotmail.com>
2026-03-31 16:20:03 +0800

    feat: add header to OrderBook and center component layout with responsive styling

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,7 @@ import OrderBook from "./components/OrderBook/OrderBook.vue";
 </script>
 
 <template>
-  <div id="app">
-    <OrderBook />
-  </div>
+  <OrderBook />
 </template>
 
 <style>
@@ -19,5 +17,21 @@ body {
   background-color: #131b29;
   color: #f0f4f8;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
+}
+
+#app {
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 24px 0;
+}
+
+@media (min-width: 480px) {
+  #app {
+    align-items: center;
+    padding: 0;
+  }
 }
 </style>
--- src/components/OrderBook/OrderBook.vue
+++ src/components/OrderBook/OrderBook.vue
@@ -21,6 +21,7 @@ onUnmounted(() => {
 
 <template>
   <div class="order-book">
+    <div class="header">Order Book</div>
     <QuoteTable type="sell" />
     <LastPrice />
     <QuoteTable type="buy" />
@@ -28,9 +29,24 @@ onUnmounted(() => {
 </template>
 
 <style scoped>
+.header {
+  width: 100%;
+  font-size: 16px;
+  font-weight: 600;
+  text-align: left;
+  padding: 8px;
+  border-bottom: 1px solid #2a364a;
+}
 .order-book {
-  width: 280px;
+  width: 100%;
   background-color: #131b29;
   color: #f0f4f8;
+  padding: 24px;
+}
+
+@media (min-width: 480px) {
+  .order-book {
+    width: 320px;
+  }
 }
 </style>
